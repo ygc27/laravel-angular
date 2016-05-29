ecdf6f3b490791fa9a536e530a3b7207fc8be57c
Yasmany G Casanova <ygc_21@hotmail.com>
2016-05-29 16:49:39 -0300

    Upload Projeto fase 2 Front-End

---
+++ resources/assets/js/controllers/login.js
@@ -0,0 +1,27 @@
+angular.module('app.controllers')
+
+        .controller('LoginController', ['$scope', '$location', 'OAuth', function ($scope, $location, OAuth) {
+                $scope.user = {
+                    username: '',
+                    password: ''
+                };
+                
+                $scope.error = {
+                    message: '',
+                    error: false
+                };
+
+                $scope.login = function () {
+
+                    if ($scope.form.$valid) {
+
+                        OAuth.getAccessToken($scope.user).then(function () {
+                            $location.path('home');
+                        }, function (data) {
+                            //alert('Login invalido');
+                            $scope.error.error = true;
+                            $scope.error.message = data.data.error_description;
+                        });
+                    }
+                };
+            }]);
