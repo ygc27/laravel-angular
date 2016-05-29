c81bbc991bc339472303b038a25a9829623c614b
Yasmany G Casanova <ygc_21@hotmail.com>
2016-05-29 10:08:58 -0300

    Atualização do arquivo route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
 Route::post('oauth/access_token', function() {
@@ -12,19 +12,7 @@ Route::group(['middleware' => 'oauth'], function() {
 
     Route::resource('client', 'ClientController', ['except' => ['create', 'edit']]);
 
-    /* Route::group(['middleware' => 'CheckProjectOwner'], function() {
-      Route::resource('project', 'ProjectController', ['except' => ['create', 'edit']]);
-      }); */
-
     Route::resource('project', 'ProjectController', ['except' => ['create', 'edit']]);
-
-    /*
-      Route::get('client', ['middleware' => 'oauth', 'uses' => 'ClientController@index']);
-      Route::post('client', 'ClientController@store');
-      Route::get('client/{id}', 'ClientController@show');
-      Route::put('client/{id}', 'ClientController@update');
-      Route::delete('client/{id}', 'ClientController@destroy');
-     */
 
     Route::group(['prefix' => 'project'], function() {
 
@@ -33,24 +21,9 @@ Route::group(['middleware' => 'oauth'], function() {
         Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
         Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
         Route::delete('{id}/note/{noteId}', 'ProjectNoteController@destroy');
-        
+
         Route::post('{id}/file', 'ProjectFileController@store');
     });
-
-    /*
-      Route::get('project/{id}/note', 'ProjectNoteController@index');
-      Route::post('project/{id}/note', 'ProjectNoteController@store');
-      Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-      Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-      Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
-     */
-
-    /* Route::get('project', 'ProjectController@index');
-      Route::post('project', 'ProjectController@store');
-      Route::get('project/{id}', 'ProjectController@show');
-      Route::put('project/{id}', 'ProjectController@update');
-      Route::delete('project/{id}', 'ProjectController@destroy');
-     */
 });
 
 
